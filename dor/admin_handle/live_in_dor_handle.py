5a881373cbdadd47fe91b2a51dab8eef62c2fb0a
14myhe <694135826@qq.com>
2017-09-20 10:03:58 +0800

    整合彩君的代码。目前无bug，还剩下入宿处理

--- dor/admin_handle/live_in_dor_handle.py
+++ dor/admin_handle/live_in_dor_handle.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 
 def ad_add_stu_dor_info(request):
-    return render(request, "admin/checkIn.html")
+    username=request.session['username']
+    return render(request, "admin/checkIn.html",{'username':username})
 def ad_distribute_dor(request):
     pass
